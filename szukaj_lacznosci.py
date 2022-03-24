b1ae70da7382abbb1e87ee3245c368c3cc4d85e3
Jacek Wielemborek <d33tah@gmail.com>
2022-03-24 19:06:36 +0100

    Bugfix

--- szukaj_lacznosci.py
+++ szukaj_lacznosci.py
@@ -16,16 +16,26 @@ MIN_SYGNAL_DB = -68
 MIN_DLUGOSC_S = 20
 
 
+def moze_wyswietl_lacznosc(k, v, ts, lacznosc_od):
+    dlugosc = (ts - lacznosc_od[v]) if lacznosc_od[v] else 0
+    if lacznosc_od[v] is not None and dlugosc > MIN_DLUGOSC_S:
+        print(
+            k,
+            str(datetime.datetime.fromtimestamp(ts)),
+            dlugosc,
+        )
+
+
 def main():
     fin = open("/dev/stdin", "rb")
     lacznosc_od = {v: None for v in OFFSETY.values()}
     n = 0
     while True:
-        ts = fin.read(8)
-        if len(ts) < 8:
+        ts_b = fin.read(8)
+        if len(ts_b) < 8:
             break
         n += 1
-        ts = struct.unpack("d", ts)[0]
+        ts = struct.unpack("d", ts_b)[0]
         data = fin.read(4 * 4096)
         floats = struct.unpack("f" * 4096, data)
         for k, v in OFFSETY.items():
@@ -33,14 +43,10 @@ def main():
                 if lacznosc_od[v] is None:
                     lacznosc_od[v] = ts
             elif floats[v] < MIN_SYGNAL_DB:
-                dlugosc = (ts - lacznosc_od[v]) if lacznosc_od[v] else 0
-                if lacznosc_od[v] is not None and dlugosc > MIN_DLUGOSC_S:
-                    print(
-                        k,
-                        str(datetime.datetime.fromtimestamp(ts)),
-                        dlugosc,
-                    )
+                moze_wyswietl_lacznosc(k, v, ts, lacznosc_od)
                 lacznosc_od[v] = None
+    for k, v in OFFSETY.items():
+        moze_wyswietl_lacznosc(k, v, ts, lacznosc_od)
     sys.stderr.write("Przeanalizowano %d probek.\n" % n)
 
 
