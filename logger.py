656959d9738e0dfab6bf755c982748e4181f8c92
Jacek Wielemborek <d33tah@gmail.com>
2022-03-24 19:09:06 +0100

    Simplify logger.py.

--- logger.py
+++ logger.py
@@ -8,44 +8,6 @@ while True:
     try:
         ws = create_connection("ws://192.168.1.134:8073/ws/")
         ws.send("SERVER DE CLIENT client=openwebrx.js type=receiver")
-        ws.send(
-            """{
-              "type": "connectionproperties",
-              "params": {
-                "output_rate": 11025,
-                "hd_output_rate": 44100
-              }
-            }"""
-        )
-        ws.send(
-            """{
-          "type": "dspcontrol",
-          "params": {
-            "low_cut": -4000,
-            "high_cut": 4000,
-            "offset_freq": -400000,
-            "mod": "nfm",
-            "dmr_filter": 3,
-            "squelch_level": -50,
-            "secondary_mod": false
-          }
-        }"""
-        )
-        ws.send(
-            """{
-          "type": "dspcontrol",
-          "action": "start"
-        }"""
-        )
-        ws.send(
-            """{
-          "type": "dspcontrol",
-          "params": {
-            "secondary_offset_freq": 1000
-          }
-        }"""
-        )
-
         n = 0
         t = time.time()
         while True:
